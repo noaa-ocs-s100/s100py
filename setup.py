099ebe411fc12aae4f21554b6bbdf0ff34dc65c1
Jason Greenlaw <jason.greenlaw@noaa.gov>
2019-02-14 17:07:05 +0000

    Use standard os.path convention

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import setuptools
-from os import path
+import os
 import pathlib
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
 
 # Get the long description from the README file
-with open(path.join(HERE, 'README.rst'), encoding='utf-8') as f:
+with open(os.path.join(HERE, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setuptools.setup(
