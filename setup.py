37750b036c0e0bed125d13307bbb8884f45353bd
Erin Nagel <Erin.Nagel@noaa.gov>
2019-02-14 11:02:47 -0500

    Added setup script

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+from os import path
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# Get the long description from the README file
+with open(path.join(HERE, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="s100py",
+    version="0.1.0",
+    license="",
+    author="Erin Nagel, Jason Greenlaw",
+    author_email="erin.nagel@noaa.gov, jason.greenlaw@noaa.gov",
+    description="Convert Oceanographic Operational Forecast System Datasets into IHO S-100/S-111 Format",
+    long_description=long_description,
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'Intended Audience :: Science/Research',
+        "License :: OSI Approved",
+        "Operating System :: OS Independent",
+    ],
+)
