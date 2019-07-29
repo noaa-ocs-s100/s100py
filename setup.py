e52a8aa42fda4ebc87f3e881a651f0315c661093
erin-nagel <Erin.Nagel@noaa.gov>
2019-07-29 17:29:31 -0400

    Modify setup.py

--- setup.py
+++ setup.py
@@ -6,24 +6,25 @@ import pathlib
 HERE = pathlib.Path(__file__).parent
 
 # Get the long description from the README file
-with open(os.path.join(HERE, 'README.rst'), encoding='utf-8') as f:
+with open(os.path.join(HERE, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setuptools.setup(
     name='s100py',
-    version='0.2.0',
-    license='',
+    version='0.3.0',
+    license='BSD-2-Clause',
     author='Erin Nagel, Jason Greenlaw',
     author_email='erin.nagel@noaa.gov, jason.greenlaw@noaa.gov',
-    description='Convert Oceanographic Operational Forecast System Datasets into IHO S-100/S-111 Format',
+    description='This python package provides utilities for encoding hydrographic datasets in the International Hydrographic Organization (IHO) S-100 format',
     long_description=long_description,
-    url='',
+    long_description_content_type="text/markdown",
+    url='https://github.com/noaa-ocs-s100/s100py',
     packages=setuptools.find_packages(),
-    install_requires=['thyme(>=0.2.0)', 'numpy', 'h5py'],
+    install_requires=['thyme(>=0.3.3)', 'numpy', 'h5py'],
     classifiers=[
         'Programming Language :: Python :: 3',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved',
+        'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
     ],
 )
