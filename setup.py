e1be765b52eaa7760ef7b33e7dea2b1b64b56939
erin-nagel <Erin.Nagel@noaa.gov>
2020-01-24 12:11:34 -0500

    Upgrade version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(HERE, 'README.md'), encoding='utf-8') as f:
 
 setuptools.setup(
     name='s100py',
-    version='0.4.0',
+    version='0.5.0',
     license='BSD-2-Clause',
     author='Erin Nagel, Jason Greenlaw',
     author_email='erin.nagel@noaa.gov, jason.greenlaw@noaa.gov',
